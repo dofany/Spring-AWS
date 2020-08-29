aa760c0b1797438dd3a595fe552e741ebb72377b
dofany <kimdohwan17@gmail.com>
2020-08-29 17:37:44 +0900

    JPA 실행 및 테스트

---
+++ src/main/java/com/dofany/dofanyhomepage/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.dofany.dofanyhomepage.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
