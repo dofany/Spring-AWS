4bca43e286b3898a19432d5f7dcead59b5253d6d
dofany <kimdohwan17@gmail.com>
2020-08-30 17:48:14 +0900

    BaseTimeEntity 클래스 상속

--- src/main/java/com/dofany/dofanyhomepage/domain/posts/Posts.java
+++ src/main/java/com/dofany/dofanyhomepage/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.dofany.dofanyhomepage.domain.posts;
 
+import com.dofany.dofanyhomepage.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,5 +34,4 @@ public class Posts {
         this.title = title;
         this.content = content;
     }
-
 }
