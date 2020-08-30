4a653306c73043f9bce59cff7c7376170dfa0fb0
dofany <kimdohwan17@gmail.com>
2020-08-30 19:30:46 +0900

    서비스

--- src/main/java/com/dofany/dofanyhomepage/service/posts/PostsService.java
+++ src/main/java/com/dofany/dofanyhomepage/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.dofany.dofanyhomepage.service.posts;
 
 import com.dofany.dofanyhomepage.domain.posts.Posts;
 import com.dofany.dofanyhomepage.domain.posts.PostsRepository;
+import com.dofany.dofanyhomepage.web.dto.PostsListResponseDto;
 import com.dofany.dofanyhomepage.web.dto.PostsResponseDto;
 import com.dofany.dofanyhomepage.web.dto.PostsSaveRequestDto;
 import com.dofany.dofanyhomepage.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()
         -> new IllegalArgumentException("해당 게시글이 없습니다. id = " +id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
