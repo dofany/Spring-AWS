83ae9ec335e8d1b1232d1b0206877906dc0dc05f
dofany <kimdohwan17@gmail.com>
2020-08-30 19:29:17 +0900

    IndexController 생성

---
+++ src/main/java/com/dofany/dofanyhomepage/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.dofany.dofanyhomepage.web;
+
+import com.dofany.dofanyhomepage.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+        private final PostsService postsService;
+        @GetMapping("/")
+        public String index(Model model){
+            model.addAttribute("posts",postsService.findAllDesc());
+            return "index";
+    }
+}
